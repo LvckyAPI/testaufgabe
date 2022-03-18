afeb057f26dce165e637eafc9b70bd648808f4fe
Iven S <stossenhd@gmail.com>
2022-03-18 15:43:09 +0000

    add a check if a space is free in play()

--- app/app/App/Http/Controllers/GameController.php
+++ app/app/App/Http/Controllers/GameController.php
@@ -176,14 +176,16 @@ class GameController extends Controller
         // $game->setSpace( $x, $y, GameMark::Circle ).
         // [ The code to check if the space is free goes here ]
 
-        // If the space is not free, run the code in the line below by removing the //
-        //return response("This space has already been claimed!")->setStatusCode(403)->header('Content-Type', 'text/plain');
 
-        // [ The code to update the game board goes here ]
+        if (!($game->getSpace($x, $y) === GameMark::None)) {
+            return response("This space has already been claimed!")->setStatusCode(403)->header('Content-Type', 'text/plain');
+        } else {
+            $game->setSpace($x, $y, GameMark::Circle);
+        }
 
-        // Saving the game board and output it to the player
+        // Save the game board
         $game->save();
-        return $this->status_output( $game );
+        return $this->status_output($game);
     }
 
     /**
